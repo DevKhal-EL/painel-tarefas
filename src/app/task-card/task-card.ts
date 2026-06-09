9444ec4c16e7028646ce45c468a2da12e6d898ce
Benicio Khalel <beniciokhalel@gmail.com>
2026-06-09 20:19:55 -0300

    Ajuste no layout do painel e estilos

--- src/app/task-card/task-card.ts
+++ src/app/task-card/task-card.ts
@@ -1,14 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { Task } from '../task.model';
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './task-card.html',
   styleUrl: './task-card.scss',
 })
 export class TaskCard {
-
   @Input() tarefa!: Task;
-
 }
